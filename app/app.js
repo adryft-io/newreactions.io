002e6fe91d127e3acdbe5fec2a82e64356d31201
Shams Ali <ali.s90@outlook.com>
2016-06-24 15:07:12 -0700

    creating boiler plate

--- app/app.js
+++ app/app.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react'
-import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory, DefaultRoute } from 'react-router'
+
 class App extends Component {
-  render() {
+  render () {
     return (
       <Router history={hashHistory}>
-        <Route path='/' component={Home} />
-        <Route path='/address' component={Address} />
-        <Route path='*' component={NotFound} />
+        <Route path='/' component={Container}>
+          <IndexRoute component={Home} />
+          <Route path='address' component={Address} />
+          <Route path='*' component={NotFound} />
+        </Route>
       </Router>
     )
   }
 }
+
+const Nav = () => (
+  <div>
+    <Link to='/'>Home</Link>&nbsp;
+    <Link to='/address'>Address</Link>
+  </div>
+)
+
+const Container = (props) => <div>
+  <Nav />
+  {props.children}
+</div>
+
 const Home = () => <h1>Hello from Home!</h1>
+
 const Address = () => <h1>We are located at 555 Jackson St.</h1>
-const NotFound = () => (<h1>404.. This page is not found!</h1>)
+const NotFound = () => <h1>404.. This page is not found!</h1>
+
 export default App

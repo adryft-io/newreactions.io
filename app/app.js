19989853d9d942eb02bf85b89088c691d975e1f2
Shams Ali <ali.s90@outlook.com>
2016-06-24 15:02:25 -0700

    initial commit

---
+++ app/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
+class App extends Component {
+  render() {
+    return (
+      <Router history={hashHistory}>
+        <Route path='/' component={Home} />
+        <Route path='/address' component={Address} />
+        <Route path='*' component={NotFound} />
+      </Router>
+    )
+  }
+}
+const Home = () => <h1>Hello from Home!</h1>
+const Address = () => <h1>We are located at 555 Jackson St.</h1>
+const NotFound = () => (<h1>404.. This page is not found!</h1>)
+export default App

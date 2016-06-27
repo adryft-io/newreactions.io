07e8a5e121522d0c0780dd9ec242112d2c3221f9
Shams Ali <ali.s90@outlook.com>
2016-06-27 16:30:21 -0700

    changes to dashboard

--- app/App.jsx
+++ app/App.jsx
@@ -5,19 +5,13 @@ import NotFound from './NotFound.jsx';
 import RecipeForm from './RecipeForm.jsx';
 import Gmail from './Gmail.jsx';
 import Login from './Login.jsx';
+import Dashboard from './components/Dashboard.jsx';
 import { Router, Route, Link, IndexRoute, hashHistory } from 'react-router';
 
 
-const Container = (props) => (
-  <div>
-    <Nav />
-    {props.children}
-  </div>
-);
+const Container = (props) => <div><Nav />{props.children}</div>;
 
-Container.propTypes = {
-  children: React.PropTypes.object,
-};
+Container.propTypes = { children: React.PropTypes.object };
 
 const App = () => (
   <Router history={hashHistory}>
@@ -27,6 +21,7 @@ const App = () => (
       <Route path="gmail" component={Gmail} />
       <Route path="login" component={Login} />
       <Route path="recipeform" component={RecipeForm} />
+      <Route path="dashboard" component={Dashboard} />
       <Route path="*" component={NotFound} />
     </Route>
   </Router>
@@ -38,7 +33,8 @@ const Nav = () => (
     <Link to="/address">Address</Link>&nbsp;
     <Link to="/recipeform">Recipe Form</Link>&nbsp;
     <Link to="/login">Login</Link>&nbsp;
-    <Link to="/gmail">Gmail</Link>
+    <Link to="/gmail">Gmail</Link>&nbsp;
+    <Link to="/dashboard">Dashboard</Link>
   </div>
 );
 
--- app/components/Dashboard.jsx
+++ app/components/Dashboard.jsx
@@ -3,23 +3,39 @@ import React, { Component } from 'react';
 export class Dashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = { count: props.initialCount };
-    this.tick = this.tick.bind(this);
+    this.state = { actions: [], reactions: [] };
+    this.getActions = this.getActions.bind(this);
+    this.getReactions = this.getReactions.bind(this);
   }
-  tick() {
-    this.setState({ count: this.state.count + 1 });
+  getActions() {
+    this.setState({ actions: this.state.actions.push('action') });
+  }
+  getReactions() {
+    this.setState({ reactions: this.state.reactions.push('rx') });
   }
   render() {
     return (
-      <div onClick={this.tick}>
-        <h3>Welcome Shams,
+      <div>
+        <span className="divider">
+          <h3>Welcome Shams,</h3>
           <p>Here you can view all your created actions and reactions.
           You may filter by the type of service you are using.</p>
+        </span>
+        <h3>
+          <ul>
+            <li>
+              {this.props.actions}
+            </li>
+          </ul>
+          <ul>
+            <li>
+              {this.props.reactions}
+            </li>
+          </ul>
         </h3>
-        Clicks: {this.state.count}
       </div>
     );
   }
 }
-Dashboard.propTypes = { initialCount: React.PropTypes.number };
-Dashboard.defaultProps = { initialCount: 0 };
+Dashboard.propTypes = { actions: React.PropTypes.text, reactions: React.PropTypes.text };
+Dashboard.defaultProps = { actions: ['hi im action'], reactions: ['hi im reaction'] };

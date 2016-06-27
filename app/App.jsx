2debdec64e4bc797f8bbf8d3826b80a05b700fc7
Esther Cuan <esthercuan@gmail.com>
2016-06-27 16:50:29 -0700

    created html structure for dashboard

--- app/App.jsx
+++ app/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Home from './Home.jsx';
-import Address from './Address.jsx';
 import NotFound from './NotFound.jsx';
 import RecipeForm from './RecipeForm.jsx';
 import Gmail from './Gmail.jsx';
@@ -9,11 +8,32 @@ import Dashboard from './components/Dashboard.jsx';
 import Container from './Container.jsx';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
+
+
+const Container = (props) => (
+  <div>
+    <h4>new reaction logo</h4>
+    {props.children}
+    <h4>weather goes here </h4>
+    <ul>
+      <li><Link to="/login">Log in</Link></li>
+      <li><Link to="/manifesto">manifesto</Link></li>
+      <li><Link to="/about">about</Link></li>
+      <li><Link to="/dashboard">Dashboard</Link></li>
+    </ul>
+  </div>
+);
+// /login should change to log out when the user is loged in
+
+Container.propTypes = {
+  children: React.PropTypes.object,
+};
+
 const App = () => (
   <Router history={hashHistory}>
     <Route path="/" component={Container}>
       <IndexRoute component={Home} />
-      <Route path="address" component={Address} />
+      <Route path="dashboard" component={Dashboard} />
       <Route path="gmail" component={Gmail} />
       <Route path="login" component={Login} />
       <Route path="recipeform" component={RecipeForm} />
@@ -22,5 +42,4 @@ const App = () => (
     </Route>
   </Router>
 );
-
 export default App;
---
+++ app/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Sidebar = (props) => (
+  <div>
+    {props.children}
+  </div>
+);
+
+export default Sidebar;

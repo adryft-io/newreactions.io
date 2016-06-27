d09a43e28be2cbfbb728b8f5a26c76b4fabe0ecb
Shams Ali <ali.s90@outlook.com>
2016-06-27 16:43:11 -0700

    modularized contianer and nav

--- app/App.jsx
+++ app/App.jsx
@@ -6,12 +6,8 @@ import RecipeForm from './RecipeForm.jsx';
 import Gmail from './Gmail.jsx';
 import Login from './Login.jsx';
 import Dashboard from './components/Dashboard.jsx';
-import { Router, Route, Link, IndexRoute, hashHistory } from 'react-router';
-
-
-const Container = (props) => <div><Nav />{props.children}</div>;
-
-Container.propTypes = { children: React.PropTypes.object };
+import Container from './Container.jsx';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 const App = () => (
   <Router history={hashHistory}>
@@ -26,17 +22,5 @@ const App = () => (
     </Route>
   </Router>
 );
-
-const Nav = () => (
-  <div>
-    <Link to="/">Home</Link>&nbsp;
-    <Link to="/address">Address</Link>&nbsp;
-    <Link to="/recipeform">Recipe Form</Link>&nbsp;
-    <Link to="/login">Login</Link>&nbsp;
-    <Link to="/gmail">Gmail</Link>&nbsp;
-    <Link to="/dashboard">Dashboard</Link>
-  </div>
-);
-
 
 export default App;

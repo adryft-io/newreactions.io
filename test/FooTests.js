5485db1d367001a3b3ff1c760b9c613004b5ef57
Shams Ali <ali.s90@outlook.com>
2016-06-27 15:44:45 -0700

    mocha chai tests are working example foo is there

---
+++ test/FooTests.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+import Foo from '../app/Foo.jsx';
+/* global describe it */
+
+describe('A suite', () => {
+  it('contains spec with an expectation', () => {
+    expect(shallow(<Foo />).contains(<div className="foo" />)).to.equal(true);
+  });
+
+  it('contains spec with an expectation', () => {
+    expect(shallow(<Foo />).is('.foo')).to.equal(true);
+  });
+
+  it('contains spec with an expectation', () => {
+    expect(mount(<Foo />).find('.foo').length).to.equal(1);
+  });
+});

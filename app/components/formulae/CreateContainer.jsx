a373ccd6d6a2965396a00153d6cd5d88f4d07bb5
Matthew Vargeson <mvargeson@Matthews-MacBook-Air.local>
2016-06-27 20:10:40 -0700

    add action and reaction selection

--- app/components/formulae/CreateContainer.jsx
+++ app/components/formulae/CreateContainer.jsx
@@ -13,6 +13,10 @@ class CreateContainer extends Component {
       actions: [],
       reactions: [],
     };
+
+    this.onSelectAction = this.onSelectAction.bind(this);
+    this.onSelectReaction = this.onSelectReaction.bind(this);
+    this.onClickConfigure = this.onClickConfigure.bind(this);
   }
 
   componentDidMount() {
@@ -32,12 +36,33 @@ class CreateContainer extends Component {
     }, 250);
   }
 
+  onSelectAction(action) {
+    console.log('action: ', action);
+    this.setState({ selectedAction: action });
+    console.log(this.state);
+  }
+
+  onSelectReaction(reaction) {
+    console.log('reaction: ', reaction);
+    this.setState({ selectedReaction: reaction });
+    console.log(this.state);
+  }
+
+  onClickConfigure() {
+    console.log('go to configuration page...');
+  }
+
   render() {
     return (
       <Create
         channel={this.state.channel}
+        selectedAction={this.state.selectedAction}
         actions={this.state.actions}
+        selectedReaction={this.state.selectedReaction}
         reactions={this.state.reactions}
+        onSelectAction={this.onSelectAction}
+        onSelectReaction={this.onSelectReaction}
+        onClickConfigure={this.onClickConfigure}
       />
     );
   }

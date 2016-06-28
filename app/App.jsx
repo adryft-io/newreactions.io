b4cea152b203e118bee0830eb50d073f74991d99
Shams Ali <ali.s90@outlook.com>
2016-06-27 17:32:15 -0700

    cleaned up files

--- app/App.jsx
+++ app/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Home from './Home.jsx';
 import NotFound from './NotFound.jsx';
-import RecipeForm from './RecipeForm.jsx';
-import Gmail from './Gmail.jsx';
 import Login from './Login.jsx';
 import Dashboard from './components/Dashboard.jsx';
 import MainContainer from './components/MainContainer.jsx';
@@ -14,10 +12,7 @@ const App = () => (
     <Route path="/" component={MainContainer}>
       <IndexRoute component={Home} />
       <Route path="dashboard" component={Dashboard} />
-      <Route path="gmail" component={Gmail} />
       <Route path="login" component={Login} />
-      <Route path="recipeform" component={RecipeForm} />
-      <Route path="dashboard" component={Dashboard} />
       <Route path="*" component={NotFound} />
     </Route>
   </Router>

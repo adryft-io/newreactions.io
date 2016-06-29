bea3096376313648c7724155cb125161583d72a2
Nam Dao <nam.d.dao@gmail.com>
2016-06-28 22:19:23 -0700

    deleted duplicates

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -9,6 +9,7 @@ const Nav = () => (
       <li><Link to="/dashboard">Dashboard</Link></li>
       <li><Link to="/formulae/create/gmail">Create Gmail Formula</Link></li>
       <li><Link to="/sign-in">Sign In</Link></li>
+      <li><Link to="/select">Select</Link></li>
     </ul>
   </div>
 );

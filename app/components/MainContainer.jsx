38a5d8d5d77c11806cdf5799d6d2baef4fe9407d
Esther Cuan <esthercuan@gmail.com>
2016-06-29 20:12:34 -0700

    Splash Page finished styling

--- app/components/MainContainer.jsx
+++ app/components/MainContainer.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Nav from './Nav.jsx';
 
-const MainContainer = (props) => <div className="row">{props.children}<Nav /></div>;
-
+const MainContainer = (props) => <div className="row">{props.children}</div>;
 MainContainer.propTypes = { children: React.PropTypes.object };
-
 export default MainContainer;

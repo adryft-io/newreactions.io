0dbd2bba904bc3104c55464ab1810a64251f3b1f
Esther Cuan <esthercuan@gmail.com>
2016-06-29 17:26:55 -0700

    applied css skeleton framework and arranged the containers into columns

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Sidebar = (props) => (
-  <div>
+  <div className="four columns">
     {props.children}
   </div>
 );
--- app/components/home/Home.jsx
+++ app/components/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Splash from '../splash/Splash.jsx';
 
 const Home = () => (
-  <div>
+  <div className="eight columns">
     <Splash
       title={'Here we create action and reaction'}
       subtitle={'Please log in to get started'}

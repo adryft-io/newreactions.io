5f0c84b3ffd65a930d80cb8f8266454e005655d9
Esther Cuan <esthercuan@gmail.com>
2016-06-29 19:09:01 -0700

    fixed the position of MainContainer

--- app/components/MainContainer.jsx
+++ app/components/MainContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Nav from './Nav.jsx';
 
-const MainContainer = (props) =><div className="row">{props.children}<Nav /></div>;
+const MainContainer = (props) => <div className="row">{props.children}<Nav /></div>;
 
 MainContainer.propTypes = { children: React.PropTypes.object };
 

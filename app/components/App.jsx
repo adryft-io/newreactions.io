73624b3f99664c0da78b4cc3d76f16e891456f76
Nam Dao <nam.d.dao@gmail.com>
2016-07-01 12:18:28 -0700

    implemented restricted routes based on valid session

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -11,20 +11,54 @@ import SelectContainer from './select/SelectContainer.jsx';
 import MainContainer from './MainContainer.jsx';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.requireAuth = this.requireAuth.bind(this);
+  }
 
-const App = () => (
-  <Router history={hashHistory}>
-    <Route path="/" component={MainContainer}>
-      <IndexRoute component={Home} />
-      <Route path="dashboard" component={DashboardContainer} />
-      <Route path="sign-in" component={SignIn} />
-      <Route path="about" component={AboutContainer} />
-      <Route path="manifesto" component={ManifestoContainer} />
-      <Route path="select" component={SelectContainer} />
-      <Route path="formulae/create/:actionChannel" component={CreateContainer} />
-      <Route path="formulae/configure/:actionId/:reactionId" component={ConfigureContainer} />
-      <Route path="*" component={NotFound} />
-    </Route>
-  </Router>
-);
+  requireAuth(nextState, replace, next) {
+    fetch('/api/v1/auth/verify', { credentials: 'include' })
+    .then(res => res.json())
+    .then(data => {
+      if (data.message === '401') {
+        replace('/sign-in');
+        next();
+      } else {
+        next();
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Router history={hashHistory}>
+        <Route path="/" component={MainContainer}>
+          <IndexRoute component={Home} />
+          <Route path="dashboard" component={DashboardContainer} onEnter={this.requireAuth} />
+          <Route path="sign-in" component={SignIn} />
+          <Route path="about" component={AboutContainer} />
+          <Route path="manifesto" component={ManifestoContainer} />
+          <Route
+            path="select"
+            component={SelectContainer}
+            onEnter={this.requireAuth}
+          />
+          <Route
+            path="formulae/create/:actionChannel"
+            component={CreateContainer}
+            onEnter={this.requireAuth}
+          />
+          <Route
+            path="formulae/configure/:actionId/:reactionId"
+            component={ConfigureContainer}
+            onEnter={this.requireAuth}
+          />
+          <Route path="*" component={NotFound} />
+        </Route>
+      </Router>
+    );
+  }
+}
+
 export default App;

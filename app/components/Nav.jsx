5525442acc281dd16cf2d3d09d62e419b483b5e1
Esther Cuan <esthercuan@gmail.com>
2016-07-01 10:08:14 -0700

    Changed navigation

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,9 +4,12 @@ import { Link } from 'react-router';
 const Nav = () => (
   <div>
     <ul>
-      <li><Link to="/sign-in">sign in</Link></li>
       <li><Link to="/manifesto">manifesto</Link></li>
       <li><Link to="/about">about</Link></li>
+      <li><Link to="/dashboard">Dashboard</Link></li>
+      <li><Link to="/formulae/create/gmail">Create Gmail Formula</Link></li>
+      <li><Link to="/sign-in">Sign In</Link></li>
+      <li><Link to="/select">Select</Link></li>
     </ul>
   </div>
 );

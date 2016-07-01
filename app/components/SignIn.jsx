6ec8829c68aea36de3025fcae62bfa0d3055845f
Nam Dao <nam.d.dao@gmail.com>
2016-06-30 21:53:44 -0700

    change api route for login button

--- app/components/SignIn.jsx
+++ app/components/SignIn.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const SignIn = () => (
   <div>
     <h3 className="sign-in-form"> Please Sign In </h3>
-    <form action="/v1/auth/connect/google" method="get">
+    <form action="api/v1/auth/connect/google" method="get">
       <button type="submit"> Sign in with Google </button>
     </form>
-    <form action="/v1/auth/connect/google" method="get">
+    <form action="api/v1/auth/connect/google" method="get">
       <button type="submit"> Sign in with Facebook </button>
     </form>
   </div>

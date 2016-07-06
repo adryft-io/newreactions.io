e4fd39d3dbf291b36de1532b10fd97ebfc5911be
Matthew Vargeson <mvargeson@Matthews-MacBook-Air.local>
2016-07-06 11:52:56 -0700

    fix labels on dashboard

--- app/components/dashboard/DashboardInfoSidebar.jsx
+++ app/components/dashboard/DashboardInfoSidebar.jsx
@@ -21,21 +21,29 @@ const DashboardInfoSidebar = ({ formulaInfo, deleteFormula }) => (
             <p className="dashSideP">{formulaInfo.name}</p>
           </li>
           <li>
-            <span className="bold">Reaction Name: </span>
-            <p className="dashSideP">{formulaInfo.reaction_name}</p>
-          </li>
-          <li>
-            <span className="bold">Created At: </span>
+            <span className="bold">Created: </span>
             <p className="dashSideP">{moment(formulaInfo.created_at).fromNow()}</p>
-          </li>
-          <li>
-            <span className="bold">Action Fields: </span>
-            {listActions(formulaInfo.reaction_fields)}
           </li>
           <li>
             <span className="bold">Action Name: </span>
             <p className="dashSideP">{formulaInfo.action_name}</p>
           </li>
+          {Object.keys(formulaInfo.action_fields).length > 0 ?
+            <li>
+              <span className="bold">Action Fields: </span>
+              {listActions(formulaInfo.action_fields)}
+            </li> : null
+          }
+          <li>
+            <span className="bold">Reaction Name: </span>
+            <p className="dashSideP">{formulaInfo.reaction_name}</p>
+          </li>
+          {Object.keys(formulaInfo.reaction_fields).length > 0 ?
+            <li>
+              <span className="bold">Reaction Fields: </span>
+              {listActions(formulaInfo.reaction_fields)}
+            </li> : null
+          }
           <a
             className="button"
             onClick={() => (confirm(`Delete formula ${formulaInfo.name}?`) ?

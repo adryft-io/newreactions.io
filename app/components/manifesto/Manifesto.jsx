be36227bb748bfd01ea22066481682befa6c4a71
Esther Cuan <esthercuan@gmail.com>
2016-07-06 15:56:48 -0700

    added logo, float and fixed minor issues

--- app/components/manifesto/Manifesto.jsx
+++ app/components/manifesto/Manifesto.jsx
@@ -5,27 +5,33 @@ const Manifesto = () => (
     <h3>Manifesto</h3>
     <div className="six columns">
       <p className="bio">
-        The manifesto first column Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-        dolor in reprehenderit in voluptate velit esse cillum dolore eu
-        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-         sunt in culpa qui officia deserunt mollit anim id est laborum. Donec
-         elementum ligula eu sapien consequat eleifend.
+        We are committed to the internet of things because
+        we believe in the future of technology.
+        At this point in history humans have become incredibly
+        good at sharing, storing and retrieving information. Now
+        it is time to turn information into action, where
+        technologies start to detect and communicate information
+        wirelessly in order to provoke reaction.
+        Consider the collapse of a bridge in Minnesota back in 2007
+        which costed the deaths of many people. Now we can
+        rebuild it with smart cement: cement equipped with sensors that can monitor stresses,
+        cracks and warpages. It can also alert if there
+        is ice on the bridge, or if there is a structural
+        problem that could be a hazard for potential users.
       </p>
       <span className="divider" />
     </div>
     <div className="five columns">
       <p className="bio">
-        The manifesto second column Lorem ipsum dolor sit amet,
-        consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-        dolor in reprehenderit in voluptate velit esse cillum dolore eu
-        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-         sunt in culpa qui officia deserunt mollit anim id est laborum. Donec
-         elementum ligula eu sapien consequat eleifend.
+        This cement would communicate wirelessly with your
+        car which in turn would alert the driver that
+        there is a hazard ahead. The way IoT transitions
+        information to action is a fundamental shift in
+        the way technology impacts our lives. When we
+        start making smart objects that can gather
+        information and turn it into action, we can now
+        have a major engine for creating new products and services.
+        The internet of things is the next step on the technological ladder.
       </p>
       <span className="divider" />
     </div>

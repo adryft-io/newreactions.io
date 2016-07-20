5d5ba3ca862f2b3c5b35627ae9bad68c7ab75aac
Shams Ali <ali.s90@outlook.com>
2016-07-20 17:37:40 -0500

    removed underscore and updated to lodash

--- app/components/dashboard/DashboardHelpers.js
+++ app/components/dashboard/DashboardHelpers.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import _ from 'underscore';
+import _ from 'lodash';
 
 
 const getUniqueChannels = (data, element) => _.uniq(_.reduce(data.data, (prev, curr) => {
--- app/components/dashboard/DashboardInfoSidebar.jsx
+++ app/components/dashboard/DashboardInfoSidebar.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 import moment from 'moment';
-import _ from 'underscore';
+import _ from 'lodash';
 import { listActions } from './DashboardHelpers.js';
 
 

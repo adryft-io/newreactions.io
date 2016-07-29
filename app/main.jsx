41683db3a49f824ea4b737ad395bc2dacf24a74e
Shams Ali <ali.s90@outlook.com>
2016-07-29 16:35:32 -0500

    removed bower and used css-loaders instead

--- app/main.jsx
+++ app/main.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import '../node_modules/skeleton-css/css/skeleton.css';
+import '../node_modules/skeleton-css/css/normalize.css';
+import './css/styles.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
+// ERROR in ./~/css-loader!./app/css/styles.css
+// css:5:1: Unknown word
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var proxy = require('express-http-proxy');
+const express = require('express');
+const proxy = require('express-http-proxy');
 
-var app = express();
+const app = express();
 
 app.use(express.static('./app'));
 app.use('/api', proxy(process.env.WEB_SERVER_URL));
 app.listen(process.env.PORT || 8100);
-
